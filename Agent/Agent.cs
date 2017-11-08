e00e27716d37e527ce0f2f3ba69d95b8dd79d1a9
Jordan Best <j_best_11@hotmail.com>
2017-11-08 08:19:42 -0600

    Commiting Agent

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -6,13 +6,45 @@ namespace IntelligentVacuum.Agent
 
     public class Agent
     {
+        int prevYAxis = -1;
+        int prevXAxis = -1;
+        bool moveRight = true;
         public Agent()
         {
         }
 
         public AgentAction DecideAction(Room room)
         {
-            return AgentAction.NONE;
+            AgentAction action;
+            int currentXAxis = room.XAxis;
+            int currentYAxis = room.YAxis;
+            bool isDirty = room.IsDirty;
+            if(isDirty)
+            {
+                action = AgentAction.CLEAN;
+            }
+            else
+            {
+                if(currentXAxis == prevXAxis && currentYAxis == prevYAxis)
+                {
+                    action = AgentAction.MOVE_DOWN;
+                    moveRight = !moveRight;
+                }
+                else
+                {
+                    if(moveRight)
+                    {
+                        action = AgentAction.MOVE_RIGHT;
+                    }
+                    else
+                    {
+                        action = AgentAction.MOVE_LEFT;
+                    }
+                }
+                prevXAxis = currentXAxis;
+                prevYAxis = currentYAxis;
+            }   
+            return action;
         }
     }
 }
